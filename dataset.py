27b3d7d4c2744e0e3f9c01dd7529e5a386ee9088
Chetan Srinidhi <srinidhipy@gmail.com>
2021-02-16 20:16:28 -0500

    "added Camelyon16_test_loader"

--- dataset.py
+++ dataset.py
@@ -938,6 +938,63 @@ class DatasetCamelyon16_eval(Dataset):
 
         return img, label
 
+#################
+
+class DatasetCamelyon16_test(Dataset):                           # Final Predictions #
+
+    def __init__(self, data_path, mask_path, image_size):
+
+            """
+            Camelyon16 dataset class wrapper
+                    data_path: string, path to pre-sampled images
+                    json_path: string, path to the annotations in json format
+            """
+
+            self.data_path = data_path
+            self.mask_path = mask_path
+            self.image_size = image_size
+            self.preprocess()
+
+    def preprocess(self):
+
+        self.mask = np.load(self.mask_path)
+        self.slide = openslide.OpenSlide(self.data_path)
+
+        X_slide, Y_slide = self.slide.level_dimensions[0]
+        X_mask, Y_mask = self.mask.shape
+
+        if round(X_slide / X_mask) != round(Y_slide / Y_mask):
+            raise Exception('Slide/Mask dimension does not match ,'
+                            ' X_slide / X_mask : {} / {},'
+                            ' Y_slide / Y_mask : {} / {}'
+                            .format(X_slide, X_mask, Y_slide, Y_mask))
+
+        self.resolution = round(X_slide * 1.0 / X_mask)
+        if not np.log2(self.resolution).is_integer():
+            raise Exception('Resolution (X_slide / X_mask) is not power of 2 :'
+                            ' {}'.format(self.resolution))
+
+        # all the indices for tissue region from the tissue mask
+        self.X_idcs, self.Y_idcs = np.where(self.mask)
+
+    def __len__(self):
+        return len(self.X_idcs)
+
+    def __getitem__(self, idx):
+
+        x_mask, y_mask = self.X_idcs[idx], self.Y_idcs[idx]
+
+        x_center = int((x_mask + 0.5) * self.resolution)
+        y_center = int((y_mask + 0.5) * self.resolution)
+
+        x = int(x_center - self.image_size / 2)
+        y = int(y_center - self.image_size / 2)
+
+        img = self.slide.read_region((x, y), 0, (self.image_size, self.image_size)).convert('RGB')
+        img = np.array(img, dtype=np.float32).transpose((2, 0, 1))
+
+        return img, x_mask, y_mask
+
 ##########################################################################
 
 ###### Kather dataloaders #############
